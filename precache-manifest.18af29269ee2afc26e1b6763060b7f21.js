af5668f0d3fb0d29894603ab23d24f02f6fc05df
Andrey Zinoviev <sttrog_810@mail.ru>
2022-10-16 15:13:34 +0300

    Updates

---
+++ precache-manifest.18af29269ee2afc26e1b6763060b7f21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "015f4fa64f3588c9bf07",
+    "url": "/static/css/main.b1cdb9ed.chunk.css"
+  },
+  {
+    "revision": "015f4fa64f3588c9bf07",
+    "url": "/static/js/main.015f4fa6.chunk.js"
+  },
+  {
+    "revision": "f993ed0ea40465f97e7e",
+    "url": "/static/js/1.f993ed0e.chunk.js"
+  },
+  {
+    "revision": "118fdab5cf4ea731122b",
+    "url": "/static/js/2.118fdab5.chunk.js"
+  },
+  {
+    "revision": "d895deb6de546ffabfe8",
+    "url": "/static/js/runtime~main.d895deb6.js"
+  },
+  {
+    "revision": "4c1b845b93a5c5e26fb0340ef96cef96",
+    "url": "/static/media/sova_logo_icon.4c1b845b.png"
+  },
+  {
+    "revision": "88212ec7503ac78c554602e71adea151",
+    "url": "/index.html"
+  }
+];
