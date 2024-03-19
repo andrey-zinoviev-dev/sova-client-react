de081cad1a4077328c49bdc0e75160f103db0fef
Andrey <sttrog_810@mail.ru>
2024-03-19 11:15:13 -0700

    highlighting active contact

--- src/components/LessonContent.js
+++ src/components/LessonContent.js
@@ -6,7 +6,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faArrowRight, faPaperPlane, faPaperclip } from "@fortawesome/free-solid-svg-icons"
 import { UserContext } from "../context/userContext"
 import { useSearchParams } from "react-router-dom";
-import { apiGetConversation } from "../api"
+import { apiGetConversation, apiSendMessage } from "../api"
 import FilePopup from "./FilePopup"
 
 export default function LessonContent({ courseID, lesson, module, students, author }) {
@@ -20,6 +20,8 @@ export default function LessonContent({ courseID, lesson, module, students, auth
   const [messages, setMessages] = React.useState(null);
   const [errorMsg, setErrorMsg] = React.useState("");
   const [openFilePopup, setOpenFilePopup] = React.useState(false);
+  const [selectedFile, setSelectedFile] = React.useState(null);
+
   //logged in user
   const loggedInUser = React.useContext(UserContext);
   //derived state
@@ -29,10 +31,16 @@ export default function LessonContent({ courseID, lesson, module, students, auth
   })
   //refs
   const fileInputRef = React.useRef();
+  const messageInputRef = React.useRef();
 
   //functions
   function sendMessage() {
-    console.log('message sent');
+    const formData = new FormData();
+    formData.append('messageData', JSON.stringify({text: messageInputRef.current.value, from: "", to: "", location: {}}));
+    apiSendMessage(token, formData)
+    .then((data) => {
+      console.log(data);
+    })
   };
 
   function changeFileInput() {
@@ -42,10 +50,10 @@ export default function LessonContent({ courseID, lesson, module, students, auth
   React.useEffect(() => {
     userId && apiGetConversation(token, userId, {course: courseID, module: module._id, lesson: lesson._id})
     .then((data) => {
+      setChatIsOpened(true);
       if(data.message) {
         throw new Error(data.message);
       } else {
-        setChatIsOpened(true);
         setMessages(data.messages);
         setErrorMsg("");
       }
@@ -88,7 +96,7 @@ export default function LessonContent({ courseID, lesson, module, students, auth
                   <h3>Контакты</h3>
                   <ul className="lesson__div-chat-contacts">
                     {contacts.map((contact) => {
-                      return <li className="lesson__div-chat-contacts-li" key={contact.name}>
+                      return <li style={{backgroundColor: contact._id === selectedUser._id && "#5DB0C7"}} className="lesson__div-chat-contacts-li" key={contact.name}>
                         <Contact contact={contact} />
                       </li>
                     })}
@@ -112,10 +120,14 @@ export default function LessonContent({ courseID, lesson, module, students, auth
                       </ul>
                       <form onSubmit={(evt) => {
                         evt.preventDefault();
+                        // formData.append('file', )
                         sendMessage();
+                        // console.log(messageInputRef.current.value, messageInputRef.current.name)
+                        // console.log(evt.target);
+                        // sendMessage(evt.target);
                       }}>
                         <input onChange={changeFileInput} ref={fileInputRef} type="file" style={{display: "none"}}></input>
-                        <input placeholder="Написать сообщение..."></input>
+                        <input ref={messageInputRef} name="message" placeholder="Написать сообщение..."></input>
                         <button type="button" onClick={() => {
                           fileInputRef.current.click();
                         }}>
